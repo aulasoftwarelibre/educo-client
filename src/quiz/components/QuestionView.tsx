584c9e6506ca91050f7f9dbaa91ec190d004608d
Victor Monserrat <victormonserratvillatoro@gmail.com>
2019-04-04 20:54:52 +0200

    Question view sizes styled

--- src/quiz/components/QuestionView.tsx
+++ src/quiz/components/QuestionView.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Button, Grid, Header } from 'semantic-ui-react';
+import { Button, Grid } from 'semantic-ui-react';
 import styled from 'styled-components';
 import { PropsFromDispatch, PropsFromState } from '../containers/QuestionView';
 
@@ -9,60 +9,96 @@ export const QuestionView = ({
     answer2,
     answer3,
 }: Props): JSX.Element =>
-    <Grid>
+    <StyledGrid>
         <Grid.Row>
             <Grid.Column>
-                <Header>
+                <Question>
                     { question }
-                </Header>
+                </Question>
             </Grid.Column>
         </Grid.Row>
         <Grid.Row>
             <Grid.Column>
-                <RedButton
-                    fluid
-                    size={'massive'}
-                >
+                <RedButton>
                     { answer1 }
                 </RedButton>
             </Grid.Column>
         </Grid.Row>
         <Grid.Row>
             <Grid.Column>
-                <BlueButton
-                    fluid
-                    size={'massive'}
-                >
+                <BlueButton>
                     { answer2 }
                 </BlueButton>
             </Grid.Column>
         </Grid.Row>
         <Grid.Row>
             <Grid.Column>
-                <YellowButton
-                    fluid
-                    size={'massive'}
-                >
+                <YellowButton>
                     { answer3 }
                 </YellowButton>
             </Grid.Column>
         </Grid.Row>
-    </Grid>
+    </StyledGrid>
 ;
 
 export type Props = PropsFromState & PropsFromDispatch;
 
+const StyledGrid = styled(Grid)`
+    margin: 0 !important;
+`;
+
+const Question = styled.h1`
+    font-size: 3vh;
+    font-weight: 900;
+    padding: 2vh 10vw;
+    text-align: justify;
+    
+    @media only screen and (orientation:landscape) {
+        font-size: 6vh; 
+    }
+`;
+
 const RedButton = styled(Button)`
-    background: #a90132 !important;
+    background: #a6192e !important;
     color: #ffffff !important;
+    width: 80vw;
+    height: 8vh;
+    font-size: 3vh !important;
+    padding: 0 !important;
+    margin: 0 10vw !important;
+    
+    @media only screen and (orientation:landscape) {
+        height: 12vh;
+        font-size: 6vh !important;
+    }
 `;
 
 const BlueButton = styled(Button)`
-    background: #241b5c !important;
+    background: #211551 !important;
     color: #ffffff !important;
+    width: 80vw;
+    height: 8vh;
+    font-size: 3vh !important;
+    padding: 0 !important;
+    margin: 0 10vw !important;
+    
+    @media only screen and (orientation:landscape) {
+        height: 12vh;
+        font-size: 6vh !important;
+    }
 `;
 
 const YellowButton = styled(Button)`
-    background: #f1b310 !important;
+    background: #f6be00 !important;
     color: #ffffff !important;
+    width: 80vw;
+    height: 8vh;
+    font-size: 3vh !important;
+    padding: 0 !important;
+    margin: 0 10vw !important;
+    
+    @media only screen and (orientation:landscape) {
+        height: 12vh;
+        font-size: 6vh !important;
+    }
 `;
