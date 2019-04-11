7c0b43b02ee84e5401ea770894f3c3a5b2b032b8
Victor Monserrat <victormonserratvillatoro@gmail.com>
2019-04-11 12:21:37 +0200

    Replacing grid component by styled component

--- src/layout/components/Grid.tsx
+++ src/layout/components/Grid.tsx
@@ -2,18 +2,6 @@ import * as React from 'react';
 import { Grid as SemanticGrid } from 'semantic-ui-react';
 import styled from 'styled-components';
 
-export const Grid = ({
-    children
-}: Props): JSX.Element =>
-    <StyledGrid>
-        { children }
-    </StyledGrid>
-;
-
-export interface Props {
-    children: React.ReactNode
-}
-
-const StyledGrid = styled(SemanticGrid)`
+export const Grid = styled(SemanticGrid)`
     margin: 0 !important;
 `;
