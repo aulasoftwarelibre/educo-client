48577b2145365a331d02e1107926cb78436fe626
Victor Monserrat <victormonserratvillatoro@gmail.com>
2019-04-17 12:14:20 +0200

    Showing correct answer in stats view

--- src/stats/components/StatsView.tsx
+++ src/stats/components/StatsView.tsx
@@ -1,33 +1,53 @@
 import * as React from 'react';
+import styled from 'styled-components';
 import { PropsFromDispatch, PropsFromState } from '../containers/StatsView';
 import { Column, Grid, Row } from '../../layout/components/Grid';
 import { Question } from '../../quiz/components/Question';
 import { Stat } from './Stat';
+import { colorsPalette, PaletteColor } from '../../utils/colors-palette';
 
 export const StatsView = ({
     question: { content, answers },
-}: Props): JSX.Element =>
-    <Grid>
-        <Row>
-            <Column>
-                <Question>
-                    { content }
-                </Question>
-            </Column>
-        </Row>
-        { answers.map(({ rate }, index) =>
-            <Row key={ index }>
-                <Column key={ index }>
-                    <Stat
-                        key={ index }
-                        color={ index % 3 === 0 ? 'red' : index % 3 === 1 ? 'blue' : 'yellow' }
-                        percent={ Math.round(rate!) }
-                        progress
-                    />
+}: Props): JSX.Element => {
+
+    const correctIndex = answers.findIndex(({ correct }) => correct!);
+
+    return(
+        <Grid>
+            <Row>
+                <Column>
+                    <Question>
+                        { content }
+                        <Span color={ correctIndex % 3 === 0 ? 'red' : correctIndex % 3 === 1 ? 'blue' : 'yellow' }>
+                            {` ${answers[correctIndex].content}`}
+                        </Span>
+                        .
+                    </Question>
                 </Column>
             </Row>
-        )}
-    </Grid>
-;
+            { answers.map(({ content, correct, rate }, index) =>
+                <Row key={ index }>
+                    <Column key={ index }>
+                        <Stat
+                            key={ index }
+                            color={ index % 3 === 0 ? 'red' : index % 3 === 1 ? 'blue' : 'yellow' }
+                            disabled={ !correct }
+                            percent={ Math.round(rate!) }
+                            progress
+                        />
+                    </Column>
+                </Row>
+            )}
+        </Grid>
+    );
+};
 
 export type Props = PropsFromState & PropsFromDispatch;
+
+const Span = styled.span`
+    color: ${(props: SpanProps) => colorsPalette[props.color]};
+`;
+
+interface SpanProps {
+    color: PaletteColor,
+}
