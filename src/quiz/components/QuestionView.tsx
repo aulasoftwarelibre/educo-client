155bac3974d61d72c5bf555f1fce9cd872fa2ed8
Sergio Gómez <sergio@uco.es>
2019-04-24 19:54:15 +0200

    Added transitions to views

--- src/quiz/components/QuestionView.tsx
+++ src/quiz/components/QuestionView.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { Transition } from 'semantic-ui-react';
 import { PropsFromDispatch, PropsFromState } from '../containers/QuestionView';
 import { Column, Grid, Row } from '../../layout/components/Grid';
 import { Countdown } from './Countdown';
@@ -9,13 +10,14 @@ export const QuestionView = ({
     question: { content, durationSeconds, elapsedSeconds, answers },
     voteAnswerWithId,
 }: Props): JSX.Element => {
+    const [visible, setVisible] = React.useState(false);
     const [remainingSeconds, setRemainingSeconds] = React.useState(
         durationSeconds - Math.ceil(elapsedSeconds)
     );
 
     React.useEffect(() => {
         const interval = setInterval(() => {
-            if(remainingSeconds < 1) {
+            if (remainingSeconds < 1) {
                 return;
             }
 
@@ -25,42 +27,54 @@ export const QuestionView = ({
         return () => clearInterval(interval);
     });
 
-    return(
-        <Grid>
-            { remainingSeconds > 0 &&
-                <Row>
-                    <Column>
-                        <Countdown
-                            value={ remainingSeconds }
-                            total={ durationSeconds }
-                            progress={'value'}
-                            indicating
-                        />
-                    </Column>
-                </Row>
-            }
-            <Row>
-                <Column>
-                    <Question>
-                        { content }
-                    </Question>
-                </Column>
-            </Row>
-            { answers.map(({ id, content }, index) =>
-                <Row key={ index }>
-                    <Column key={ index }>
-                        <Answer
-                            key={ index }
-                            color={ index % 3 === 0 ? 'red' : index % 3 === 1 ? 'blue' : 'yellow' }
-                            disabled={ remainingSeconds < 1 }
-                            onClick={ () => voteAnswerWithId(id) }
-                        >
-                            { content }
-                        </Answer>
-                    </Column>
-                </Row>
+    React.useEffect(() => {
+        setVisible(true);
+    }, []);
+
+    return (
+        <Transition.Group animation={'fade down'} duration={2000}>
+            {visible && (
+                <Grid>
+                    {remainingSeconds > 0 && (
+                        <Row>
+                            <Column>
+                                <Countdown
+                                    value={remainingSeconds}
+                                    total={durationSeconds}
+                                    progress={'value'}
+                                    indicating
+                                />
+                            </Column>
+                        </Row>
+                    )}
+                    <Row>
+                        <Column>
+                            <Question>{content}</Question>
+                        </Column>
+                    </Row>
+                    {answers.map(({ id, content }, index) => (
+                        <Row key={index}>
+                            <Column key={index}>
+                                <Answer
+                                    key={index}
+                                    color={
+                                        index % 3 === 0
+                                            ? 'red'
+                                            : index % 3 === 1
+                                            ? 'blue'
+                                            : 'yellow'
+                                    }
+                                    disabled={remainingSeconds < 1}
+                                    onClick={() => voteAnswerWithId(id)}
+                                >
+                                    {content}
+                                </Answer>
+                            </Column>
+                        </Row>
+                    ))}
+                </Grid>
             )}
-        </Grid>
+        </Transition.Group>
     );
 };
 
--- src/stats/components/StatsView.tsx
+++ src/stats/components/StatsView.tsx
@@ -1,44 +1,66 @@
 import * as React from 'react';
 import styled from 'styled-components';
+import { Transition } from 'semantic-ui-react';
 import { PropsFromDispatch, PropsFromState } from '../containers/StatsView';
 import { Column, Grid, Row } from '../../layout/components/Grid';
 import { Question } from '../../quiz/components/Question';
 import { Stat } from './Stat';
 import { colorsPalette, PaletteColor } from '../../utils/colors-palette';
 
-export const StatsView = ({
-    question: { content, answers },
-}: Props): JSX.Element => {
+export const StatsView = ({ question: { content, answers } }: Props): JSX.Element => {
+    const [visible, setVisible] = React.useState(false);
+
+    React.useEffect(() => {
+        setVisible(true);
+    }, []);
 
     const correctIndex = answers.findIndex(({ correct }) => correct!);
 
-    return(
-        <Grid>
-            <Row>
-                <Column>
-                    <Question>
-                        { content }
-                        <Span color={ correctIndex % 3 === 0 ? 'red' : correctIndex % 3 === 1 ? 'blue' : 'yellow' }>
-                            {` ${answers[correctIndex].content}`}
-                        </Span>
-                        .
-                    </Question>
-                </Column>
-            </Row>
-            { answers.map(({ content, correct, rate }, index) =>
-                <Row key={ index }>
-                    <Column key={ index }>
-                        <Stat
-                            key={ index }
-                            color={ index % 3 === 0 ? 'red' : index % 3 === 1 ? 'blue' : 'yellow' }
-                            disabled={ !correct }
-                            percent={ Math.round(rate!) }
-                            progress
-                        />
-                    </Column>
-                </Row>
+    return (
+        <Transition.Group animation={'fade down'} duration={2000}>
+            {visible && (
+                <Grid>
+                    <Row>
+                        <Column>
+                            <Question>
+                                {content}
+                                <Span
+                                    color={
+                                        correctIndex % 3 === 0
+                                            ? 'red'
+                                            : correctIndex % 3 === 1
+                                            ? 'blue'
+                                            : 'yellow'
+                                    }
+                                >
+                                    {` ${answers[correctIndex].content}`}
+                                </Span>
+                                .
+                            </Question>
+                        </Column>
+                    </Row>
+                    {answers.map(({ content, correct, rate }, index) => (
+                        <Row key={index}>
+                            <Column key={index}>
+                                <Stat
+                                    key={index}
+                                    color={
+                                        index % 3 === 0
+                                            ? 'red'
+                                            : index % 3 === 1
+                                            ? 'blue'
+                                            : 'yellow'
+                                    }
+                                    disabled={!correct}
+                                    percent={Math.round(rate!)}
+                                    progress
+                                />
+                            </Column>
+                        </Row>
+                    ))}
+                </Grid>
             )}
-        </Grid>
+        </Transition.Group>
     );
 };
 
@@ -49,5 +71,5 @@ const Span = styled.span`
 `;
 
 interface SpanProps {
-    color: PaletteColor,
+    color: PaletteColor;
 }
