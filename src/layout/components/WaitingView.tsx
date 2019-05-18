6d0e72ab1cbbd25aaabdc72e40b26593c924992e
Sergio Gómez <decano@gmail.com>
2019-05-18 12:59:43 +0200

    Added transitions to views (#2)
    * Added transitions to views
    
    * Change html title
    
    * Change row key in QuestionView

--- src/layout/components/WaitingView.tsx
+++ src/layout/components/WaitingView.tsx
@@ -1,40 +1,42 @@
 import * as React from 'react';
 import styled from 'styled-components';
+import { Transition } from 'semantic-ui-react';
 import { Column, Grid, Row } from './Grid';
 import { colorsPalette, PaletteColor } from '../../utils/colors-palette';
 
-export const WaitingView = ({
-}: Props): JSX.Element =>
-    <Grid>
-        <Row>
-            <Column width={16}>
-                <Title>
-                    ed
-                    <Span color={'red'}>
-                        U
-                    </Span>
-                    <Span
-                        color={'blue'}
-                        stretched
-                    >
-                        C
-                    </Span>
-                    <Span color={'yellow'}>
-                        O
-                    </Span>
-                </Title>
-            </Column>
-            <Column width={16}>
-                <Subtitle>
-                    GALA DE VIDA UNIVERSITARIA Y DEPORTE
-                </Subtitle>
-            </Column>
-        </Row>
-    </Grid>
-;
+export const WaitingView = ({  }: Props): JSX.Element => {
+    const [visible, setVisible] = React.useState(false);
 
-export interface Props {
-}
+    React.useEffect(() => {
+        setVisible(true);
+    }, []);
+
+    return (
+        <Grid>
+            <Transition.Group animation={'fade down'} duration={2000}>
+                {visible && [
+                    <Row key="row">
+                        <Column width={16}>
+                            <Title>
+                                ed
+                                <Span color={'red'}>U</Span>
+                                <Span color={'blue'} stretched>
+                                    C
+                                </Span>
+                                <Span color={'yellow'}>O</Span>
+                            </Title>
+                        </Column>
+                        <Column width={16}>
+                            <Subtitle>GALA DE VIDA UNIVERSITARIA Y DEPORTE</Subtitle>
+                        </Column>
+                    </Row>,
+                ]}
+            </Transition.Group>
+        </Grid>
+    );
+};
+
+export interface Props {}
 
 const Title = styled.h1`
     text-align: center;
@@ -49,10 +51,10 @@ const Subtitle = styled.h2`
 
 const Span = styled.span`
     color: ${(props: SpanProps) => colorsPalette[props.color]};
-    letter-spacing: ${(props: SpanProps) => props.stretched ? '-1vw' : '-0.25vw'};
+    letter-spacing: ${(props: SpanProps) => (props.stretched ? '-1vw' : '-0.25vw')};
 `;
 
 interface SpanProps {
-    color: PaletteColor,
-    stretched?: boolean,
+    color: PaletteColor;
+    stretched?: boolean;
 }
